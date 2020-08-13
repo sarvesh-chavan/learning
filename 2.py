efd24e472ca1e3bc3cf1b29981f7abeea28924d9
sarvesh <sarvesh.chavan@mosambee.in>
2020-08-13 20:50:41 +0530

    save

---
+++ 2.py
@@ -0,0 +1,13 @@
+print("second")
+print("third")
+##################
+Git 
+git --version
+git config --global user.name "sarvesh"
+git config --global user.email "sarvesh.chavan@mosambee.in"
+git logs
+git commit -m "file name" "descriptive msg"
+git status
+git diff 
+git init
+############
